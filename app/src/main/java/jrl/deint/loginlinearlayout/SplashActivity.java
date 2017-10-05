784a8a41f339963e44aacd4ba731bf6f0a7645a0
jroldan686 <jroldan686@gmail.com>
2017-10-05 12:59:51 +0200

    Inicial

--- app/src/main/java/jrl/deint/loginlinearlayout/SplashActivity.java
+++ app/src/main/java/jrl/deint/loginlinearlayout/SplashActivity.java
@@ -1,13 +1,29 @@
 package jrl.deint.loginlinearlayout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView imgvImagen;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        imgvImagen = (ImageView)findViewById(R.id.imageView);
+        imgvImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent();
+                intent.setClass(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
